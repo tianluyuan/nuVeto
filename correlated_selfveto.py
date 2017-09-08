1c4075376ec6178366df7fe3511979e4d08535de
Carlos Arguelles <caad@mit.edu>
2017-09-08 14:36:25 -0500

    energy loss formulaeS

--- correlated_selfveto.py
+++ correlated_selfveto.py
@@ -6,6 +6,17 @@ from mceq_config import config, mceq_config_without
 r_dict["kaon"]=0.046
 r_dict["pion"]=0.573
 
+mass_dict["kaon"]=0.493677 # GeV
+mass_dict["pion"]=0.139570 # GeV
+
+lifetime_dict["kaon"]=1.2389e-8 # s
+lifetime_dict["pion"]=2.6033e-8 # s
+
+a["ice"]=0.249 # GeV/mwe
+a["rock"]=0.221 # GeV/mwe
+b["ice"]=0.422e-3 # 1/mwe
+b["rock"]=0.531e-3 # 1/mwe
+
 def GetRunMCLayeredMode(theta,hadronic_model='SIBYLL-2.3c',primary_model=(pm.HillasGaisser2012, 'H3a')):
     cfg = dict(config)
     mceq_run = MCEqRun(
@@ -42,6 +53,19 @@ def MinimumMesonParentEnergy(neutrino_energy,meson):
         raise Exception("Meson not found in mass dictionary.")
     r = r_dict[meson]
     return neutrino_energy/(1.-r)
+
+def DecayProbability(primary_energy, distance, meson):
+    boost_factor=primary_energy/mass_dict[meson]
+    return np.exp(-distance/(boost_factor*lifetime_dict[meson])
+
+def InteractionProbability(primary_energy, distance, meson):
+
+
+def MuonDistance(muon_energy, medium = "ice"):
+    a_ = a[medium]
+    b_ = b[medium]
+
+    return np.log(1.+ muon_energy*b_/a_)/b_
 
 def CorrelatedProbability(neutrino_energy,costh):
 
