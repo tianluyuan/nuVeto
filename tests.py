117a498373e62033c9140d58d68c23868a00dc20
Tianlu Yuan <tianlutheman@gmail.com>
2017-09-11 16:28:12 -0500

    better colors

--- tests.py
+++ tests.py
@@ -4,10 +4,11 @@ from matplotlib import pyplot as plt
 
 def test_pr(cos_theta=1, kind='numu', pmods=(), **kwargs):
     ens = np.logspace(2,9, 100)
-    plt.plot(ens, [passing_rate(en, cos_theta, kind=kind, accuracy=20, pmods=pmods) for en in ens], **kwargs)
+    pr = plt.plot(ens, [passing_rate(en, cos_theta, kind=kind, accuracy=20, pmods=pmods) for en in ens], **kwargs)
     plt.xlim(10**3, 10**7)
     plt.ylim(0, 1)
     plt.xscale('log')
+    return pr[0]
 
 
 def test_barr(cos_theta=1, kind='numu'):
@@ -15,7 +16,7 @@ def test_barr(cos_theta=1, kind='numu'):
                   (321, 'y1', 0.3),(321, 'y2', 0.3),(321, 'z', 0.122)),
                  ((211, 'h1', -0.15),(211, 'h2', -0.15),(211, 'i', -0.122),
                   (321, 'y1', -0.3),(321, 'y2', -0.3),(321, 'z', -0.122))]
+    pr = test_pr(cos_theta, kind)
     for pmods in all_pmods:
-        test_pr(cos_theta, kind, pmods, color='0.75')
+        test_pr(cos_theta, kind, pmods, color=pr.get_color(), alpha=1-abs(pmods[0][-1]))
 
-    test_pr(cos_theta, kind)
