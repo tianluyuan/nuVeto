3fe69fc88b38ecee0caa01438affff06c84a64f4
Carlos Arguelles <caad@mit.edu>
2017-09-11 20:11:38 -0500

    more functions\!

--- correlated_selfveto.py
+++ correlated_selfveto.py
@@ -1,7 +1,11 @@
 import numpy as np
+import scipy.interpolate as interpolate
 from MCEq.core import MCEqRun
+from MCEq.data import HadAirCrossSections
 import CRFluxModels as pm
 from mceq_config import config, mceq_config_without
+
+r_dict ={}; mass_dict = {}; lifetime_dict = {}; a = {}; b = {}; pdg_id = {}; air_xs_inter = {};
 
 r_dict["kaon"]=0.046
 r_dict["pion"]=0.573
@@ -16,6 +20,14 @@ a["ice"]=0.249 # GeV/mwe
 a["rock"]=0.221 # GeV/mwe
 b["ice"]=0.422e-3 # 1/mwe
 b["rock"]=0.531e-3 # 1/mwe
+
+pdg_id["kaon"] = 321 # k+
+pdg_id["pion"] = 211 # pi+
+
+cs_db = HadAirCrossSections('SIBYLL2.1')
+
+air_xs_inter["kaon"] = interpolate.interp1d(cs_db.egrid,cs_db.get_cs(pdg_id['kaon'])) # input GeV return cm^2
+air_xs_inter["pion"] = interpolate.interp1d(cs_db.egrid,cs_db.get_cs(pdg_id['pion'])) # input GeV return cm^2
 
 def GetRunMCLayeredMode(theta,hadronic_model='SIBYLL-2.3c',primary_model=(pm.HillasGaisser2012, 'H3a')):
     cfg = dict(config)
@@ -33,6 +45,7 @@ def GetRelativeContributions(mceq_run):
     total_numu = mceq_run.get_solution('total_numu', 0, grid_idx=0)
     pion_prob = mceq_run.get_solution('pi_numu', 0, grid_idx=0)/total
     kaon_prob = mceq_run.get_solution('kaon_numu', 0, grid_idx=0)/total
+    return pion_prob,kaon_prob
 
 def MinimumMuonBrotherEnergy(neutrino_energy,meson):
     """
@@ -55,20 +68,30 @@ def MinimumMesonParentEnergy(neutrino_energy,meson):
     return neutrino_energy/(1.-r)
 
 def DecayProbability(primary_energy, distance, meson):
+    if not(meson in r_dict):
+        raise Exception("Meson not found lifetime dictionary.")
     boost_factor=primary_energy/mass_dict[meson]
-    return np.exp(-distance/(boost_factor*lifetime_dict[meson])
+    return np.exp(-distance/(boost_factor*lifetime_dict[meson]))
 
-def InteractionProbability(primary_energy, distance, meson):
-
+def InteractionProbability(primary_energy, column_density, meson):
+    if not(meson in r_dict):
+        raise Exception("Meson not found cross section dictionary.")
+    return np.exp(-column_density/air_xs_inter[meson]/mass_dict[meson])
 
 def MuonDistance(muon_energy, medium = "ice"):
+    if not(medium in a) or not (medium in b):
+        raise Exception("Medium energy losses for muons not found.")
     a_ = a[medium]
     b_ = b[medium]
 
     return np.log(1.+ muon_energy*b_/a_)/b_
 
 def CorrelatedProbability(neutrino_energy,costh):
+    return 1.
 
 if __name__ == "__main__":
     mierda = GetRunMCLayeredMode(30.)
-    print "hola"
+    seccion_de_choque = cs_db.get_cs(pdg_id['pion'])
+    print seccion_de_choque
+    seccion_de_choque = cs_db.get_cs(pdg_id['kaon'])
+    print seccion_de_choque
