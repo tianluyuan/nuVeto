0491fc43c871a7d95182f3593f8436962f468bd8
Carlos Arguelles <caad@mit.edu>
2017-09-14 13:59:04 -0500

    moved things around

--- mceqveto.py
+++ mceqveto.py
@@ -13,7 +13,8 @@ from scipy.integrate import simps
 from MCEq.core import MCEqRun
 import CRFluxModels as pm
 from mceq_config import config, mceq_config_without
-
+from barr_uncertainties import ParamInfo, BARR
+from geometry import overburden
 
 SETUP = {'flux':pm.HillasGaisser2012,
          'gen':'H3a',
@@ -28,41 +29,6 @@ MCEQ = MCEqRun(
     theta_deg = 0.,
     # expand the rest of the options from mceq_config.py
     **config)
-
-
-# Global Barr parameter table format
-# ParamInfo([(x_min, x_max, E_min, E_max), ...], relative error, pdg) | x is x_lab= E_pi/E, E
-# projectile-air interaction energy
-ParamInfo = namedtuple('ParamInfo', 'regions error pdg')
-BARR = {
-    'a': ParamInfo([(0.0, 0.5, 0.00, 8.0)], 0.1, 211),
-    'b1': ParamInfo([(0.5, 1.0, 0.00, 8.0)], 0.3, 211),
-    'b2': ParamInfo([(0.6, 1.0, 8.00, 15.0)], 0.3, 211),
-    'c': ParamInfo([(0.2, 0.6, 8.00, 15.0)], 0.1, 211),
-    'd1': ParamInfo([(0.0, 0.2, 8.00, 15.0)], 0.3, 211),
-    'd2': ParamInfo([(0.0, 0.1, 15.0, 30.0)], 0.3, 211),
-    'd3': ParamInfo([(0.1, 0.2, 15.0, 30.0)], 0.1, 211),
-    'e': ParamInfo([(0.2, 0.6, 15.0, 30.0)], 0.05, 211),
-    'f': ParamInfo([(0.6, 1.0, 15.0, 30.0)], 0.1, 211),
-    'g': ParamInfo([(0.0, 0.1, 30.0, 1e11)], 0.3, 211),
-    'h1': ParamInfo([(0.1, 1.0, 30.0, 500.)], 0.15, 211),
-    'h2': ParamInfo([(0.1, 1.0, 500.0, 1e11)], 0.15, 211),
-    'i': ParamInfo([(0.1, 1.0, 500.0, 1e11)], 0.122, 211),
-    'w1': ParamInfo([(0.0, 1.0, 0.00, 8.0)], 0.4, 321),
-    'w2': ParamInfo([(0.0, 1.0, 8.00, 15.0)], 0.4, 321),
-    'w3': ParamInfo([(0.0, 0.1, 15.0, 30.0)], 0.3, 321),
-    'w4': ParamInfo([(0.1, 0.2, 15.0, 30.0)], 0.2, 321),
-    'w5': ParamInfo([(0.0, 0.1, 30.0, 500.)], 0.4, 321),
-    'w6': ParamInfo([(0.0, 0.1, 500., 1e11)], 0.4, 321),
-    'x': ParamInfo([(0.2, 1.0, 15.0, 30.0)], 0.1, 321),
-    'y1': ParamInfo([(0.1, 1.0, 30.0, 500.)], 0.3, 321),
-    'y2': ParamInfo([(0.1, 1.0, 500., 1e11)], 0.3, 321),
-    'z': ParamInfo([(0.1, 1.0, 500., 1e11)], 0.122, 321),
-    'ch_a': ParamInfo([(0.0, 0.1, 0., 1e11)], 0.1, 411),
-    'ch_b': ParamInfo([(0.1, 1.0, 0., 1e11)], 0.7, 411),
-    'ch_e': ParamInfo([(0.1, 1.0, 800., 1e11)], 0.25, 411)
-}
-
 
 def barr_unc(xmat, egrid, pname, value):
     """Implementation of hadronic uncertainties as in Barr et al. PRD 74 094009 (2006)
@@ -102,27 +68,6 @@ def amu(particle):
     :returns: the atomic mass of particle
     """
     return 1 if particle==14 else particle/100
-
-
-def overburden(cos_theta, depth=1950, elevation=2400):
-    """Returns the overburden for a detector at *depth* below some surface
-    at *elevation*.
-
-    From law of cosines,
-    x^2 == r^2+(r-d)^2-2r(r-d)cos(gamma)
-    where
-    r*cos(gamma) = r-d+x*cos(theta), solve and return x.
-
-    :param cos_theta: cosine of zenith angle 
-    :param depth:     depth of detector (in meters below the surface)
-    :param elevation: elevation of the surface above sea level (meters)
-    """
-    d = depth
-    r = 6356752. + elevation
-    z = r-d
-
-    return np.sqrt(z**2*cos_theta**2+d*(2*r-d))-z*cos_theta
-
 
 def minimum_muon_energy(distance):
     """
