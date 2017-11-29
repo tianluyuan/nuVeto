99decab8569f6f7bc2e00907485249ae38e2dc5a
Tianlu Yuan <tianlutheman@gmail.com>
2017-11-28 22:12:33 -0600

    cleaning up ipython notebook code. refactored useful fn to utils

--- selfveto.py
+++ selfveto.py
@@ -3,14 +3,46 @@ import tqdm
 import scipy.integrate as integrate
 import scipy.interpolate as interpolate
 import matplotlib.pyplot as plt
+from MCEq.core import MCEqRun
+import CRFluxModels as pm
+from mceq_config import config, mceq_config_without
 import correlated_selfveto as cs
-import utils
-from utils import Units
 from external import elbert, selfveto
+from utils import *
 
 
-def GetReachingRescaledYields(costh, dNdEE_Interpolator, e_grid, caca):
-    ice_distance = utils.overburden(costh)
+SETUP = {'flux':pm.HillasGaisser2012,
+         'gen':'H3a',
+         'hadr':'SIBYLL2.3c'}
+MCEQ = MCEqRun(
+    # provide the string of the interaction model
+    interaction_model=SETUP['hadr'],
+    # primary cosmic ray flux model
+    # support a tuple (primary model class (not instance!), arguments)
+    primary_model=(SETUP['flux'], SETUP['gen']),
+    # zenith angle \theta in degrees, measured positively from vertical direction
+    theta_deg = 0.,
+    # expand the rest of the options from mceq_config.py
+    **config)
+
+
+def get_dNdEE(mother, daughter):
+    ihijo = 20
+    e_grid = MCEQ.e_grid
+    delta = MCEQ.e_widths
+    x_range = (e_grid / e_grid[ihijo])**-1
+    dN = MCEQ.ds.get_d_matrix(mother, daughter)[ihijo]
+    dNdEE = dN*e_grid/delta
+    end_value = dNdEE[(x_range <= 1.) & (x_range >= 1.0e-3)][-1]
+    return dN, interpolate.interp1d(
+        x_range[(x_range <= 1.) & (x_range >= 1.e-3)],
+        dNdEE[(x_range <= 1.) & (x_range >= 1.e-3)],
+        bounds_error=False, fill_value=(end_value, 0.0))
+
+
+def get_reaching(costh, dNdEE_Interpolator):
+    e_grid = MCEQ.e_grid
+    ice_distance = overburden(costh)
     RY_matrix = np.zeros((len(e_grid), len(e_grid)))
     for ipadre in tqdm.tqdm(range(len(e_grid))):
         for ihijo in range(len(e_grid)):
@@ -36,114 +68,55 @@ def GetReachingRescaledYields(costh, dNdEE_Interpolator, e_grid, caca):
 
             RY_matrix[ihijo][ipadre] = integrate.dblquad(
                 lambda Ep, Enu: (dNdEE_Interpolator(
-                    Enu / Ep) / Ep) * (1. - caca.MuonReachProbability((Ep - Enu) * Units.GeV, ice_distance)),
+                    Enu / Ep) / Ep) * (1. - muon_reach_prob((Ep - Enu) * Units.GeV, ice_distance)),
                 EnuMin, EnuMax,
                 lambda Enu: np.max([Enu, EpMin]), lambda Enu: EpMax, epsabs=0, epsrel=1.e-2
             )[0] / ((EnuMax - EnuMin))
     return RY_matrix
 
 
-def GetRescaledYields(costh, dNdEE_Interpolator, e_grid):
-    ice_distance = utils.overburden(costh)
-    RY_matrix = np.zeros((len(e_grid), len(e_grid)))
-    for ipadre in tqdm.tqdm(range(len(e_grid))):
-        for ihijo in range(len(e_grid)):
-            # print "doing " + str(ipadre) + " " + str(ihijo)
-            if ihijo > ipadre:
-                RY_matrix[ihijo][ipadre] = 0
-                continue
-
-            if ihijo == 0 and ipadre == 0:
-                RY_matrix[ihijo][ipadre] = 0
-                continue
-
-            EnuMin = e_grid[ihijo - 1]
-            EnuMax = e_grid[ihijo]
-            EpMin = e_grid[ipadre - 1]
-            EpMax = e_grid[ipadre]
-
-            if ihijo == 0:
-                EnuMin = 10.  # GeV
-
-            if ipadre == 0:
-                EpMin = 10.  # GeV
-
-            RY_matrix[ihijo][ipadre] = integrate.dblquad(
-                lambda Ep, Enu: (dNdEE_Interpolator(Enu / Ep) / Ep),
-                EnuMin, EnuMax,
-                lambda Enu: np.max([Enu, EpMin]), lambda Enu: EpMax, epsabs=0, epsrel=1.e-2
-            )[0] / ((EnuMax - EnuMin))
-    return RY_matrix
-
-
-def GetPassingFraction(costh):
-    caca = cs.CorrelatedSelfVetoProbabilityCalculator(costh)
-    e_grid = caca.mceq_run.e_grid
-    delta = caca.mceq_run.e_widths
-    ihijo = 20
-    x_range = (e_grid / e_grid[ihijo])**-1
-    dNdEE = caca.mceq_run.ds.get_d_matrix(
-        caca.ParticleProperties.pdg_id["kaon"], 14)[ihijo] * e_grid / delta
-    end_value = dNdEE[(x_range <= 1.) & (x_range >= 1.0e-3)][-1]
-    dNdEE_KaonInterpolator = interpolate.interp1d(x_range[(x_range <= 1.) & (x_range >= 1.e-3)],
-                                                  dNdEE[(x_range <= 1.) & (
-                                                      x_range >= 1.e-3)],
-                                                  bounds_error=False, fill_value=(end_value, 0.0))
-
-    x_range = (e_grid / e_grid[ihijo])**-1
-    dNdEE = caca.mceq_run.ds.get_d_matrix(
-        caca.ParticleProperties.pdg_id["pion"], 14)[ihijo] * e_grid / delta
-    end_value = dNdEE[(x_range <= 1.) & (x_range >= 1.0e-3)][-1]
-    dNdEE_PionInterpolator = interpolate.interp1d(x_range[(x_range <= 1.) & (x_range >= 1.e-3)],
-                                                  dNdEE[(x_range <= 1.) & (
-                                                      x_range >= 1.e-3)],
-                                                  bounds_error=False, fill_value=(end_value, 0.0))
+def passing_rate(costh, kind='numu'):
+    dN_kaon, dNdEE_kaon = get_dNdEE(ParticleProperties.pdg_id['kaon'],
+                                    ParticleProperties.pdg_id[kind])
+    dN_pion, dNdEE_pion = get_dNdEE(ParticleProperties.pdg_id['pion'],
+                                    ParticleProperties.pdg_id[kind])
 
     print "Calculating rescaled kaon yield"
-    # KaonToNeutrinoYield=caca.mceq_run.ds.get_d_matrix(caca.ParticleProperties.pdg_id["kaon"],14)
-    KaonToNeutrinoYield = GetRescaledYields(costh, dNdEE_KaonInterpolator, e_grid)
-    rescale_kaon_decay_matrix = GetReachingRescaledYields(
-        costh, dNdEE_KaonInterpolator, e_grid, caca)
+    dN_kaon_reach = get_reaching(costh, dNdEE_kaon)
     print "Calculating rescaled pion yield"
-    # PionToNeutrinoYield=caca.mceq_run.ds.get_d_matrix(caca.ParticleProperties.pdg_id["pion"],14)
-    PionToNeutrinoYield = GetRescaledYields(costh, dNdEE_PionInterpolator, e_grid)
-    rescale_pion_decay_matrix = GetReachingRescaledYields(
-        costh, dNdEE_PionInterpolator, e_grid, caca)
+    dN_pion_reach = get_reaching(costh, dNdEE_pion)
 
-    passing_guy_numerator = np.zeros(len(caca.mceq_run.e_grid))
-    passing_guy_denominator = np.zeros(len(caca.mceq_run.e_grid))
+    MCEQ.set_theta_deg(np.degrees(np.arccos(costh)))
 
-    for idx, XX in enumerate(caca.Xvec):
-        if(idx >= len(caca.Xvec) - 1):
-            continue
-        height = caca.mceq_run.density_model.s_lX2h(
-            np.log(caca.Xvec[idx])) * Units.cm
-        deltah = (caca.mceq_run.density_model.s_lX2h(np.log(caca.Xvec[idx])) -
-                  caca.mceq_run.density_model.s_lX2h(np.log(caca.Xvec[idx + 1]))) * Units.cm
-        # if deltah>xmax:
-        #    xmax=deltah
+    passing_numerator = np.zeros(len(MCEQ.e_grid))
+    passing_denominator = np.zeros(len(MCEQ.e_grid))
+
+    Xvec = np.logspace(np.log10(1),
+                       np.log10(MCEQ.density_model.max_X), 1000)
+    heights = MCEQ.density_model.s_lX2h(np.log(Xvec)) * Units.cm
+    deltahs = np.diff(heights)
+    MCEQ.solve(int_grid=Xvec, grid_var="X")
+    for idx, deltah in enumerate(deltahs):
         # do for kaon
         inv_decay_length_array = (
-            caca.ParticleProperties.mass_dict["kaon"] / caca.mceq_run.e_grid * Units.GeV) * (
-            deltah / caca.ParticleProperties.lifetime_dict["kaon"])
+            ParticleProperties.mass_dict["kaon"] / MCEQ.e_grid * Units.GeV) *(
+            deltah / ParticleProperties.lifetime_dict["kaon"])
         rescale_phi = inv_decay_length_array * \
-            caca.mceq_run.get_solution("K-", 0, idx)
+            MCEQ.get_solution("K-", 0, idx)
 
-        passing_guy_numerator += (np.dot(rescale_kaon_decay_matrix, rescale_phi))
-        passing_guy_denominator += (np.dot(KaonToNeutrinoYield, rescale_phi))
+        passing_numerator += (np.dot(dN_kaon_reach, rescale_phi))
+        passing_denominator += (np.dot(dN_kaon, rescale_phi))
 
         # do for pion
         inv_decay_length_array = (
-            caca.ParticleProperties.mass_dict["pion"] / caca.mceq_run.e_grid * Units.GeV) * (
-            deltah / caca.ParticleProperties.lifetime_dict["pion"])
+            ParticleProperties.mass_dict["pion"] / MCEQ.e_grid * Units.GeV) *(
+            deltah / ParticleProperties.lifetime_dict["pion"])
         rescale_phi = inv_decay_length_array * \
-            caca.mceq_run.get_solution("pi-", 0, idx)
+            MCEQ.get_solution("K-", 0, idx)
 
-        passing_guy_numerator += (np.dot(rescale_pion_decay_matrix, rescale_phi))
-        passing_guy_denominator += (np.dot(PionToNeutrinoYield, rescale_phi))
-    return passing_guy_numerator / passing_guy_denominator
-
-
+        passing_numerator += (np.dot(dN_pion_reach, rescale_phi))
+        passing_denominator += (np.dot(dN_pion, rescale_phi))
+    return passing_numerator / passing_denominator
 def GetPassingFractionPrompt(costh):
     caca = cs.CorrelatedSelfVetoProbabilityCalculator(costh)
     e_grid = caca.mceq_run.e_grid
@@ -162,8 +135,8 @@ def GetPassingFractionPrompt(costh):
     rescale_prompt_decay_matrix = GetReachingRescaledYields(
         costh, dNdEE_DInterpolator)
 
-    passing_guy_numerator = np.zeros(len(caca.mceq_run.e_grid))
-    passing_guy_denominator = np.zeros(len(caca.mceq_run.e_grid))
+    passing_numerator = np.zeros(len(caca.mceq_run.e_grid))
+    passing_denominator = np.zeros(len(caca.mceq_run.e_grid))
 
     for idx, XX in enumerate(caca.Xvec):
         if(idx >= len(caca.Xvec) - 1):
@@ -179,6 +152,6 @@ def GetPassingFractionPrompt(costh):
         rescale_phi = inv_decay_length_array * \
             caca.mceq_run.get_solution("D-", 0, idx)
 
-        passing_guy_numerator += (np.dot(rescale_prompt_decay_matrix, rescale_phi))
-        passing_guy_denominator += (np.dot(DToNeutrinoYield, rescale_phi))
-    return passing_guy_numerator / passing_guy_denominator
+        passing_numerator += (np.dot(rescale_prompt_decay_matrix, rescale_phi))
+        passing_denominator += (np.dot(DToNeutrinoYield, rescale_phi))
+    return passing_numerator / passing_denominator
--- utils.py
+++ utils.py
@@ -1,6 +1,49 @@
 import numpy as np
 
 
+class Units(object):
+    # units
+    Na = 6.0221415e+23 # mol/cm^3
+    km = 5.0677309374099995 # km to GeV^-1 value from SQuIDS
+    cm = km*1.e-5
+    m = km*1.e-3
+    gr = 5.62e23 # gr to GeV value from SQuIDS
+    sec = 1523000.0 #$ sec to GeV^-1 from SQuIDS
+    GeV = 1
+    TeV = 1.e3*GeV
+    PeV = 1.e3*TeV
+
+
+class ParticleProperties(object):
+    r_dict ={}; mass_dict = {}; lifetime_dict = {}; pdg_id = {};
+    r_dict["kaon"]=0.046
+    r_dict["pion"]=0.573
+
+    mass_dict["kaon"]=0.493677*Units.GeV # GeV
+    mass_dict["pion"]=0.139570*Units.GeV # GeV
+    mass_dict["D"]=1.86962*Units.GeV # GeV
+    mass_dict["air"]=(14.5)*Units.GeV # GeV
+
+    lifetime_dict["kaon"]=1.2389e-8*Units.sec # s
+    lifetime_dict["pion"]=2.6033e-8*Units.sec # 
+    lifetime_dict["D"]=1.040e-12*Units.sec # seconds to usual Units
+
+    pdg_id["D"] = 411 # D+
+    pdg_id["kaon"] = 321 # k+
+    pdg_id["pion"] = 211 # pi+
+    pdg_id["numu"] = 14
+    pdg_id["nue"] = 12
+
+
+class MaterialProperties(object):
+    a = {}; b = {}; density = {};
+    a["ice"]=0.249*Units.GeV/Units.m # GeV/mwe
+    a["rock"]=0.221*Units.GeV/Units.m # GeV/mwe
+    b["ice"]=0.422e-3/Units.m # 1/mwe
+    b["rock"]=0.531e-3/Units.m # 1/mwe
+    density["ice"] = 0.9167*Units.gr/Units.cm**3 # g/cm^3
+
+
 def amu(particle):
     """
     :param particle: primary particle's corsika id
@@ -46,14 +89,18 @@ def minimum_muon_energy(distance):
     return 1e3 * np.exp(1e-3 * distance / (b) + 1e-8 * (distance**2) / c)
 
 
-class Units(object):
-    # units
-    Na = 6.0221415e+23 # mol/cm^3
-    km = 5.0677309374099995 # km to GeV^-1 value from SQuIDS
-    cm = km*1.e-5
-    m = km*1.e-3
-    gr = 5.62e23 # gr to GeV value from SQuIDS
-    sec = 1523000.0 #$ sec to GeV^-1 from SQuIDS
-    GeV = 1
-    TeV = 1.e3*GeV
-    PeV = 1.e3*TeV
+def ice_column_density(costh, depth = 1950.*Units.m):
+    return (overburden(costh, depth/Units.m, elevation=2400)*Units.m)*MaterialProperties.density["ice"]
+
+
+def muon_reach_prob(muon_energy, ice_distance):
+    if(muon_energy > minimum_muon_energy(ice_distance)*Units.GeV):
+        return 1.
+    else:
+        return 0.
+    # simplifying assumption that the muon reach distribution is a gaussian
+    # the probability that it does not reach is given by the cumnulative distribution function
+    # on the other hand the reaching probability is given by the survival distribution function
+    # the former is associated with the passing rate.
+    return stats.norm.cdf(ice_column_density/MaterialProperties.density["ice"],
+            loc=MeanMuonDistance(muon_energy),scale=np.sqrt(MeanMuonDistance(muon_energy)))
