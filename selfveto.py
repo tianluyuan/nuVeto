Updated get_dNdEE to interpolate MCEq points but fix edges to 2-body normalization

selfveto.py
@@ -31,24 +31,28 @@ def get_dNdEE(mother, daughter):
     e_grid = MCEQ.e_grid
     delta = MCEQ.e_widths
     x_range = e_grid[ihijo]/e_grid
+    rr = ParticleProperties.rr(mother, daughter)
     dN_mat = MCEQ.ds.get_d_matrix(ParticleProperties.pdg_id[mother],
                                   ParticleProperties.pdg_id[daughter])
     dNdEE = dN_mat[ihijo]*e_grid/delta
-    end_value = dNdEE[(x_range <= 1.) & (x_range >= 1.0e-3)][-1]
-    rr = ParticleProperties.rr(mother, daughter)
+    logx = np.log10(x_range)
+    logx_width = -np.diff(logx)[0]
+    good = (logx + logx_width/2 < np.log10(1-rr)) & (x_range >= 1.e-3)
     if (mother == 'pi+' and daughter == 'numu') or (mother == 'pi-' and daughter == 'antinumu'):
-        # Lipari 93, eq 85
-        dNdEE_interp = lambda x: 1/(1-rr)*(1-np.heaviside(x-1+rr, 0))
+        # pi -> numu are all 2-body
+        dNdEE_edge = 1/(1-rr)
     elif (mother == 'K+' and daughter == 'numu') or (mother == 'K-' and daughter == 'antinumu'):
-        # Lipari 93, eq 91
-        eps = ParticleProperties.mass_dict['pi+']/ParticleProperties.mass_dict['K+']
-        g = 1-8*eps**2-24*eps**4*np.log(eps)+8*eps**6-eps**8
-        dNdEE_interp = lambda x: np.nan_to_num((1-np.heaviside(x-1+rr, 0))*(1/g*((12-24*eps**2)*(1-eps**2)/2-4*(1-eps**2)**3-12*eps**4*(1-eps**2)+12*eps**4*x-(12-24*eps**2)/2*x**2+4*x**3+12*eps**4*np.log((1-x)/eps**2))*0.03352+1/(1-rr)*0.6356))
+        # K -> numu are mostly 2-body
+        dNdEE_edge = 0.6356/(1-rr)
     else:
-        dNdEE_interp = interpolate.interp1d(
-            x_range[(x_range <= 1.) & (x_range >= 1.e-3)],
-            dNdEE[(x_range <= 1.) & (x_range >= 1.e-3)],
-            bounds_error=False, fill_value=(end_value, 0.0))
+        # everything else 3-body
+        dNdEE_edge = 0.
+        
+    lower = dNdEE[(x_range < 1-rr) & (x_range >= 1.0e-3)][-1]
+    dNdEE_interp = interpolate.interp1d(
+        np.concatenate([[1-rr], x_range[good]]),
+        np.concatenate([[dNdEE_edge], dNdEE[good]]),
+        bounds_error=False, fill_value=(lower, 0.0))
     return x_range, dNdEE, dNdEE_interp
 
 

tests_selfveto.py
@@ -106,7 +106,7 @@ def test_dndee(mother, daughter):
     x_range, dNdEE, dNdEE_interp = get_dNdEE(mother, daughter)
 
     # print x_range[0], x_range[-1]
-    x_samp = np.logspace(1, -9, 1000)
+    x_samp = np.logspace(1, -9, 5000)
     c = plt.plot(x_samp, dNdEE_interp(x_samp), label = "Interpolated {} to {}".format(mother, daughter))
     plt.plot(x_range,dNdEE, '.', color=c[0].get_color(), label = "MCEq {} to {}".format(mother, daughter))
     plt.semilogx()