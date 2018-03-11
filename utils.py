4769a0149ef747c540d18f0dbaa11d64c926eaf1
Tianlu Yuan <tianlutheman@gmail.com>
2018-03-10 23:20:59 -0500

    move lambda0 to conv so that prompt=charm. where is lambdaC?

--- utils.py
+++ utils.py
@@ -147,11 +147,11 @@ def categ_to_mothers(categ, daughter):
     bar = '-bar' if 'anti' in daughter else ''
     lbar = '-bar' if 'anti' not in daughter else ''
     if categ == 'conv':
-        mothers = ['pi'+charge, 'K'+charge, 'K0L'] #K0S in uncorrelated?
+        mothers = ['pi'+charge, 'K'+charge, 'K0L', 'Lambda0'+lbar]
         if 'nue' in daughter:
-            mothers.append('K0S')
+            mothers.extend(['K0S'])
     elif categ == 'pr':
-        mothers = ['D'+charge, 'Ds'+charge, 'D0'+bar, 'Lambda0'+lbar]
+        mothers = ['D'+charge, 'Ds'+charge, 'D0'+bar]# 'LambdaC+'+bar
     else:
         mothers = [categ,]
     return mothers
