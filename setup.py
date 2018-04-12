b896c81ea9d4285ef95d7ff2e333d946c9d9c918
Tianlu Yuan <tianlutheman@gmail.com>
2018-04-12 14:11:05 -0500

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='nuVeto',
@@ -9,10 +9,18 @@ setup(
     author_email='caad@mit.edu, Sergio.Palomares.Ruiz@ific.uv.es, aschneider@icecube.wisc.edu, lwille@icecue.wisc.edu, and tyuan@icecube.wisc.edu',
     description='Package implements the formalism for calculating passing fraction as discussed in arXiv:XXXX.XXXX.',
     long_description=open('README.md').read(),
-    url='https://github.com/arguelles/SelfVeto.git',
-    packages=['nuVeto','nuVeto.external'],
-    package_dir={'nuVeto': 'nuVeto'},
-    package_data={'nuVeto':["data/decay_distributions/*.npz",'data/prpl/*.pkl','data/corsika/*.pkl']},
-    #data_files={'nuVeto':["data/decay_distributions/*.npz",'data/prpl/*.pkl','data/corsika/*.pkl']},
-    requires=['numpy', 'scipy', 'functools32', 'matplotlib', 'MCeq']
+    url='https://github.com/arguelles/nuVeto.git',
+    packages=find_packages('./'),
+    package_data={
+        'nuVeto':['data/decay_distributions/*.npz','data/prpl/*.pkl','data/corsika/*.pkl']
+    },
+    install_requires=[ 'numpy',
+                       'scipy',
+                       'functools32',
+                       'MCeq'],        
+    extras_require={
+        'plotting':  ['matplotlib'],
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
     )
