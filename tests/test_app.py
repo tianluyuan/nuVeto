01218637fa470b30fffebba4933d03b6a191bcfd
Tianlu Yuan <tianlutheman@gmail.com>
2018-04-12 12:52:39 -0500

    more unittests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import numpy as np
 from nuVeto.external import helper as exthp
 from nuVeto.external import selfveto as extsv
 from nuVeto.selfveto import *
@@ -14,3 +15,15 @@ def test_categ():
     assert SelfVeto.categ_to_mothers('conv', 'nue') == ['pi+', 'K+', 'K0L', 'K0S', 'mu+']
     assert SelfVeto.categ_to_mothers('pr', 'numu') == ['D+', 'Ds+', 'D0']
     assert SelfVeto.categ_to_mothers('pr', 'antinumu') == ['D-', 'Ds-', 'D0-bar']
+
+
+def test_costh_effective():
+    geom = Geometry(1950*Units.m)
+    cosths = np.linspace(0,1,100)
+    assert np.all(geom.cos_theta_eff(cosths)>=cosths)
+
+
+def test_overburden():
+    geom = Geometry(1950*Units.m)
+    cosths = np.linspace(0,1,100)
+    assert np.all(np.diff(geom.overburden(cosths)) < 0)
