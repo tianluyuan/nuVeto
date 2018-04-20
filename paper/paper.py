d39fede17fa80832d983e8e2651702f8a1979889
Tianlu Yuan <tianlutheman@gmail.com>
2018-04-20 16:02:11 -0500

    increase tick.labelsize. paper plots

--- paper/paper.py
+++ paper/paper.py
@@ -8,6 +8,12 @@ from matplotlib import pyplot as plt
 
 plt.style.use('paper.mplstyle')
 
+linestyles = ['-', '--', ':', '-.']
+titling = {'conv_numu':r'Conventional $\nu_\mu$',
+           'conv_nue':r'Conventional $\nu_e$',
+           'pr_numu':r'Prompt $\nu_\mu$',
+           'pr_nue':r'Prompt $\nu_e$'}
+
 
 def prpl():
     heaviside = mu.int_ef(resource_filename('nuVeto.resources.mu', 'mmc/ice.pklz'), mu.pl.pl_heaviside)
@@ -15,34 +21,49 @@ def prpl():
     plt.figure()
     plots.plot_prpl(heaviside, True, False)
     plt.legend()
-    plt.tight_layout()
+    plt.xlim(1e2, 1e8)
+    plt.ylim(1e3, 2e5)
+    plt.tight_layout(0.3)
     plt.savefig('fig/prpl_heaviside.png')
     plt.figure()
     plots.plot_prpl(sigmoid, True, False)
     plt.legend()
-    plt.tight_layout()
+    plt.xlim(1e2, 1e8)
+    plt.ylim(1e3, 2e5)
+    plt.tight_layout(0.3)
     plt.savefig('fig/prpl_sigmoid.png')
 
     
 def prpl_cbar():
-    plt.figure(figsize=(5,1))
+    plt.figure(figsize=(5,1.5))
     norm = mpl.colors.Normalize(vmin=0, vmax=1)
     cb = mpl.colorbar.ColorbarBase(plt.gca(),
                                    norm=norm, orientation='horizontal')
-    plt.tight_layout(0.8)
+    cb.set_label(r'${\cal P}_{\rm det}$')
+    plt.tight_layout(1)
     plt.savefig('fig/prpl_cbar.png')
 
 
 def compare_prpls():
     kinds = ['conv_nue', 'pr_nue', 'conv_numu', 'pr_numu']
-    cos_ths = [0.3]
+    cos_ths = [0.3, 0.5]
+    prpls = ['step_1', 'sigmoid_0.75_0.3']
+    labels = ['Heaviside', 'Sigmoid']
     for kind in kinds:
-        for cos_th in cos_ths:
-            plt.figure()
-            plots.prpls(cos_th, kind, compare=('step_1', 'sigmoid_0.75_0.1'))
-            plt.legend(['Heaviside', 'Sigmoid'])
-            plt.tight_layout()
-            plt.savefig('fig/prpls_{}_{}.eps'.format(kind, cos_th))
+        plt.figure()
+        plt.title(titling[kind])
+        for idx, prpl in enumerate(prpls):
+            for cos_th in cos_ths:
+                clabel = r'$\cos \theta_z = {}$'.format(cos_th) if idx == 0 else None
+                plots.fn(cos_th)(cos_th, kind, prpl=prpl,
+                                 label=clabel, linestyle=linestyles[idx])
+
+            plt.axvline(np.nan, color='k', linestyle=linestyles[idx],
+                        label=labels[idx])
+            plt.gca().set_prop_cycle(None)
+        plt.legend()
+        plt.tight_layout(0.3)
+        plt.savefig('fig/prpls_{}.eps'.format(kind))
 
 
 def compare_hadrs():
@@ -50,13 +71,20 @@ def compare_hadrs():
     hadrs=['DPMJET-III', 'SIBYLL2.3', 'SIBYLL2.3c']
     cos_ths = [0.3]
     for kind in kinds:
-        for cos_th in cos_ths:
-            plt.figure()
-            for hadr in hadrs:
-                plots.fn(cos_th)(cos_th, kind, hadr=hadr, label=hadr)
-            plt.legend()
-            plt.tight_layout()
-            plt.savefig('fig/hadrs_{}_{}.eps'.format(kind, cos_th))
+        plt.figure()
+        plt.title(titling[kind])
+        for idx, hadr in enumerate(hadrs):
+            for cos_th in cos_ths:
+                clabel = r'$\cos \theta_z = {}$'.format(cos_th) if idx == 0 else None
+                plots.fn(cos_th)(cos_th, kind, hadr=hadr,
+                                 label=clabel, linestyle=linestyles[idx])
+
+            plt.axvline(np.nan, color='k', linestyle=linestyles[idx],
+                        label=hadr)
+            plt.gca().set_prop_cycle(None)
+        plt.legend()
+        plt.tight_layout(0.3)
+        plt.savefig('fig/hadrs_{}.eps'.format(kind))
 
 
 def compare_pmodels():
@@ -67,10 +95,17 @@ def compare_pmodels():
                (pm.ZatsepinSokolskaya, 'default', 'ZS')]
     cos_ths = [0.3]
     for kind in kinds:
-        for cos_th in cos_ths:
-            plt.figure()
-            for pmodel in pmodels:
-                plots.fn(cos_th)(cos_th, kind, pmodel=pmodel[:-1], label=pmodel[-1])
-            plt.legend()
-            plt.tight_layout()
-            plt.savefig('fig/pmodels_{}_{}.eps'.format(kind, cos_th))
+        plt.figure()
+        plt.title(titling[kind])
+        for idx, pmodel in enumerate(pmodels):
+            for cos_th in cos_ths:
+                clabel = r'$\cos \theta_z = {}$'.format(cos_th) if idx == 0 else None
+                plots.fn(cos_th)(cos_th, kind, pmodel=pmodel[:-1],
+                                 label=clabel, linestyle=linestyles[idx])
+
+            plt.axvline(np.nan, color='k', linestyle=linestyles[idx],
+                        label=pmodel[-1])
+            plt.gca().set_prop_cycle(None)
+        plt.legend()
+        plt.tight_layout(0.3)
+        plt.savefig('fig/pmodels_{}.eps'.format(kind, cos_th))
