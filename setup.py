29717bb273a0cfa6308ceb7c0dc81bfd83eb1902
arguelles <caad@mit.edu>
2018-04-20 08:19:11 -0500

    addded missing packages

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=['numpy',
                       'scipy',
                       'functools32',
-                      'MCeq'],        
+                      'MCeq'],
     extras_require={
         'plotting':  ['matplotlib', 'pandas'],
         'resources':  ['pythia8', 'matplotlib', 'argparse', 'pandas'],
