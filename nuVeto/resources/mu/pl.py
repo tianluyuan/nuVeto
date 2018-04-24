d71ff6719253a019884d7ad34be24e6e76ad408b
Tianlu Yuan <tianlutheman@gmail.com>
2018-04-24 11:23:01 -0500

    add step_0.75

--- nuVeto/resources/mu/pl.py
+++ nuVeto/resources/mu/pl.py
@@ -1,14 +1,23 @@
 from scipy import stats
 
+
 def sigmoid(emu, center, scale):
     return stats.norm.cdf(emu, loc=center, scale=scale)
 
-def pl_heaviside(emu):
+
+def pl_step_1000(emu):
     """ heaviside at 1 TeV
     """
     return sigmoid(emu, 1e3, 1e-6)
 
-def pl_smeared(emu):
+
+def pl_step_750(emu):
+    """ heaviside at 1 TeV
+    """
+    return sigmoid(emu, 0.75e3, 1e-6)
+
+
+def pl_sigmoid_750_100(emu):
     """ sigmoid centered at 750 GeV
     """
     return sigmoid(emu, 750, 100)
