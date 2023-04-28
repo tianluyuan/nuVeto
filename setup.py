36946bb772cf38a729a8f9caeac9bd6cf1710bc3
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2023-04-28 18:33:08 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nuVeto',
-    version='2.1.5',
+    version='2.2.0',
     maintainer='Tianlu Yuan',
     maintainer_email='tyuan@icecube.wisc.edu',
     description='Package implements the formalism for calculating passing fraction as discussed in JCAP07(2018)047.',
