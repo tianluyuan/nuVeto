e1dff3a9d614d9d686db0ac748930faf06becf13
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2026-01-10 15:08:30 -0600

    improve __init__

--- nuVeto/__init__.py
+++ nuVeto/__init__.py
@@ -1,2 +1,15 @@
-__all__ = ['mu', 'utils', 'nuveto',
-           'uncertainties', 'external', 'examples']
+from . import mu
+from . import utils
+from . import uncertainties
+from . import external
+from . import examples
+from .nuveto import nuVeto, passing, fluxes
+
+__all__ = ['mu',
+           'utils',
+           'nuVeto',
+           'uncertainties',
+           'external',
+           'examples',
+           'passing',
+           'fluxes']
