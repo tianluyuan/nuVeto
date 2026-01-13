a1bfbae314b0f1df6feec0b04c840d5f72eb330c
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2026-01-13 13:36:12 -0600

    rm external and examples from namespace

--- src/nuVeto/__init__.py
+++ src/nuVeto/__init__.py
@@ -1,8 +1,6 @@
 from . import mu
 from . import utils
 from . import uncertainties
-from . import external
-from . import examples
 from . import nuveto
 from .nuveto import nuVeto, passing, fluxes, builder
 
@@ -10,8 +8,6 @@ __all__ = ['mu',
            'utils',
            'nuveto',
            'uncertainties',
-           'external',
-           'examples',
            'nuVeto',
            'passing',
            'fluxes',
